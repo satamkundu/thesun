03eb65b8aeabd62df407bf99707db9818f489b8a
Satam kundu <satamkundu67@gmail.com>
2024-01-31 20:31:33 +0530

    some modifications dome

--- js/all.js
+++ js/all.js
@@ -4,7 +4,7 @@ $(".container-topbar").html(
         <div class="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
             <div class="d-inline-flex align-items-center" style="height: 45px;">
                 <small class="me-3 text-light"><i class="fa fa-map-marker-alt me-2"></i>17, Hatem Munshi Lane, Kolkata – 700035</small>
-                <small class="me-3 text-light"><i class="fa fa-phone-alt me-2"></i>8240080402</small>
+                <small class="me-3 text-light"><i class="fa fa-phone-alt me-2"></i>7003583839 | 8777675893</small>
                 <small class="text-light"><i class="fa fa-envelope-open me-2"></i>thesun022021@gmail.com</small>
             </div>
         </div>
@@ -48,7 +48,7 @@ $(".container-footer").html(
                 <div class="col-lg-4 col-md-6 footer-about">
                     <div class="d-flex flex-column align-items-center justify-content-center text-center h-100 p-4">
                         <a href="index.html" class="navbar-brand">
-                            <h1 class="m-0 text-white"><img src="images/logo-main.png" style="width: 50%;"/></h1>
+                            <h1 class="m-0 text-white"><img src="images/logo-main.png" style="width: 70%;"/></h1>
                         </a>
                     </div>
                 </div>
@@ -68,7 +68,7 @@ $(".container-footer").html(
                             </div>
                             <div class="d-flex mb-2">
                                 <i class="bi bi-telephone text-primary me-2"></i>
-                                <p class="mb-0">8240080402</p>
+                                <p class="mb-0">7003583839 | 8777675893</p>
                             </div>
                             <div class="d-flex mt-4">
                                 <a class="btn btn-primary btn-square me-2" href="#"><i class="fab fa-twitter fw-normal"></i></a>
@@ -94,9 +94,13 @@ $(".container-footer").html(
                                 <h3 class="text-light mb-0">Products</h3>
                             </div>
                             <div class="link-animated d-flex flex-column justify-content-start">
-                                <a class="text-light mb-2" href="press.html"><i class="bi bi-arrow-right text-primary me-2"></i>Water Treatment Plant</a>
-                                <a class="text-light mb-2" href="achievements.html"><i class="bi bi-arrow-right text-primary me-2"></i>Water Treatment System</a>
-                                <a class="text-light" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Sewage Treatment Plant</a>
+                                <a class="text-light mb-2" href="press.html"><i class="bi bi-arrow-right text-primary me-2"></i>Effluent Treatment Plant</a>
+                                <a class="text-light mb-2" href="achievements.html"><i class="bi bi-arrow-right text-primary me-2"></i>Sewage Treatment Plant</a>
+                                <a class="text-light" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Iron Removal Plant</a>
+                                <a class="text-light" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>reverse osmosis plant</a>
+                                <a class="text-light" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>WTP</a>
+                                <a class="text-light" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Softener</a>
+                                <a class="text-light" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Composter</a>
                             </div>
                         </div>
                     </div>
@@ -107,7 +111,7 @@ $(".container-footer").html(
     <div class="container-fluid text-white" style="background: #061429;">
         <div class="container text-center">
             <div class="row justify-content-end">
-                <div class="col-lg-8 col-md-6">
+                <div class="col-lg-12 col-md-12">
                     <div class="d-flex align-items-center justify-content-center" style="height: 75px;">
                         <p class="mb-0">&copy; <div class="text-white border-bottom" >The Sun</div>. All Rights Reserved.</p>
                     </div>
